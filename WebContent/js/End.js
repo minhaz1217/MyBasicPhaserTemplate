0adfacfd2b83529ccfc5f17bcb8c00487d955118
ninjalol111 <ninjalol111@outlook.com>
2016-03-04 02:31:05 +0600

    7

---
+++ WebContent/js/End.js
@@ -0,0 +1,16 @@
+GameName.End = function(game){};
+/** @type Phaser.State */
+var proto = Object.create(Phaser.State);
+GameName.End.prototype = proto;
+
+GameName.End.prototype = {
+	preload: function(){
+		
+	},
+	create: function(){
+		
+	},
+	update: function(){
+		
+	}
+};
